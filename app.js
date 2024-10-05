e1f64e2fe553be1e2e8ce2a36b88239672cd6cd2
duanegero <duanegero11@gmail.com>
2024-10-04 21:16:12 -0300

    adding get team form and JS to handle and display info

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(`API KEY: ${apiKey}`); //logging API key just for testing
 const fetchTeams = async () => {
     const url = "https://api.balldontlie.io/v1/teams/"; // API endpoint
 
-    //uing fetch to get info from API, with my api key in header
+    //starting the try
     try {
         const response = await fetch(url, {
             headers: {
@@ -34,8 +34,12 @@ const fetchTeams = async () => {
     //variable to handle HTML list 
     const teamList = document.getElementById('team-list');
 
+    teamList.innerHTML = '';
+
+    const firstThirtyTeams = teams.slice(0 ,30);
+
     //loop through array of teams 
-    teams.forEach(team => {
+    firstThirtyTeams.forEach(team => {
        //display the team names 
         console.log(`Team Name: ${team.full_name}`);
 
@@ -50,5 +54,50 @@ const fetchTeams = async () => {
     console.error('Error', error); //in case of error, log error 
 }
 };
+
+//function to get team info
+const getTeam = async () => {
+    //preventing the page from resetting
+    event.preventDefault();
+
+    //getting value from form and assigning to variable
+    const teamID = document.getElementById('teamIDNumber').value;
+
+    //assigning uri to a variable
+    const url = `https://api.balldontlie.io/v1/teams/${teamID}`
+
+    //what we're going to try 
+    try{
+
+        //fetching the info and storing in variable
+        const response = await fetch(url, {
+            headers: {
+                'Authorization': apiKey 
+            }
+        });
+
+        //check for error first
+        if(!response.ok){
+            throw new Error('Error');
+        }
+
+        //storing JSON in variable 
+        const data = await response.json();
+
+        //storing data from JSON into a variable
+        const team = data.data;
+        //storing HTML element in a variable 
+        const teamInfo =  document.getElementById('team-info'); 
+
+        //filling the HTML element with data from JSON 
+        teamInfo.innerText = `Team Name: ${team.full_name}, City: ${team.city}, ${team.abbreviation}, Division: ${team.division}`
+
+    } catch(error) { //if any errors with catch them here
+        console.error('Error', error); //in case of error, log error 
+    }
+}
+
+//make getTeam globally accessible
+window.getTeam = getTeam;
 // Make fetchTeams globally accessible
 window.fetchTeams = fetchTeams;
